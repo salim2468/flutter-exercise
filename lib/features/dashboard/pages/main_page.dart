f947ca76466c868522e6d5eb9541cea8a1ee53b7
salim2468 <salimmaharjan2468@gmail.com>
2023-07-18 11:05:48 +0545

    youtube video player added

--- lib/features/dashboard/pages/main_page.dart
+++ lib/features/dashboard/pages/main_page.dart
@@ -1,16 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_exercise/common/button.dart';
 import 'package:flutter_exercise/common/text.dart';
 import 'package:flutter_exercise/features/dashboard/custom_widget/welcome_image.dart';
+
 import '../../../constant/colors.dart';
 import '../../category/custom_widget/category_widget.dart';
 import '../../exercise_level/custom_widget/exercise_leve_slider.dart';
+import '../../video/video_page.dart';
 
 class MainPage extends StatelessWidget {
   const MainPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    goToVideoPage() {
+      Navigator.of(context)
+          .push(MaterialPageRoute(builder: (context) => const VideoPage()));
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const CustomText(
@@ -21,38 +29,52 @@ class MainPage extends StatelessWidget {
         systemOverlayStyle: SystemUiOverlayStyle.light,
       ),
       body: const SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            WelcomeImage(),
-            SizedBox(
-              height: 15,
+        child: SizedBox(
+          height: double.maxFinite,
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                WelcomeImage(),
+                SizedBox(
+                  height: 15,
+                ),
+                CustomText(
+                  text: 'Start',
+                  alignment: Alignment.topLeft,
+                  fontWeight: FontWeight.w700,
+                  marginHorizontal: 6,
+                  marginVertical: 2,
+                  color: AppColor.kText,
+                ),
+                ExerciseLevelSlider(),
+                SizedBox(
+                  height: 20,
+                ),
+                CustomText(
+                  text: 'Category / Type',
+                  alignment: Alignment.topLeft,
+                  fontWeight: FontWeight.w700,
+                  marginHorizontal: 6,
+                  marginVertical: 2,
+                  color: AppColor.kText,
+                ),
+                CategoryWidget(),
+                SizedBox(
+                  height: 20,
+                ),
+                CustomText(
+                  text: 'Watch Video Tutorial',
+                  alignment: Alignment.topLeft,
+                  fontWeight: FontWeight.w700,
+                  marginHorizontal: 6,
+                  marginVertical: 2,
+                  color: AppColor.kText,
+                ),
+                VideoPage(),
+              ],
             ),
-            CustomText(
-              text: 'Start',
-              alignment: Alignment.topLeft,
-              fontWeight: FontWeight.w700,
-              marginHorizontal: 6,
-              marginVertical: 2,
-              color: AppColor.kText,
-            ),
-            ExerciseLevelSlider(),
-            SizedBox(
-              height: 20,
-            ),
-            CustomText(
-              text: 'Category / Type',
-              alignment: Alignment.topLeft,
-              fontWeight: FontWeight.w700,
-              marginHorizontal: 6,
-              marginVertical: 2,
-              color: AppColor.kText,
-            ),
-            CategoryWidget(),
-            SizedBox(
-              height: 20,
-            ),
-          ],
+          ),
         ),
       ),
     );
