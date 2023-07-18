05649c32ac717520f64e76a6c8c2a804b3b03965
salim2468 <salimmaharjan2468@gmail.com>
2023-07-18 13:41:13 +0545

    detail page redesign done

--- lib/common/detail_page.dart
+++ lib/common/detail_page.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_exercise/constant/colors.dart';
 import 'package:flutter_exercise/model/exercise/exercise.dart';
 
-import '../../../custom_widget/custom_card.dart';
-
 TextStyle textStyle = const TextStyle(
   fontSize: 17,
   fontWeight: FontWeight.w500,
@@ -15,6 +13,7 @@ class DetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title: Text(exercise.name)),
       body: SafeArea(
         child: SingleChildScrollView(
           child: Container(
@@ -22,62 +21,34 @@ class DetailPage extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                CardContainer(
-                  text: exercise.name,
-                  textStyle: const TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
-                  backgroundColor: AppColor.kBlack,
-                  margin: 10,
-                  width: double.maxFinite,
-                  boxShadow: [
-                    BoxShadow(
-                        color: AppColor.kBackground,
-                        blurRadius: 2,
-                        spreadRadius: 1),
-                  ],
-                ),
                 const SizedBox(
                   height: 10,
                 ),
-                Text(
-                  'Type: ${exercise.type}',
-                  style: textStyle,
-                ),
+                DetailPageInfoCard(text: 'Type: ${exercise.type}'),
                 const SizedBox(
                   height: 8,
                 ),
-                Text(
-                  'Muscle: ${exercise.muscle}',
-                  style: textStyle,
-                ),
+                DetailPageInfoCard(text: 'Muscle: ${exercise.muscle}'),
                 const SizedBox(
                   height: 8,
                 ),
-                Text(
-                  'Equipments: ${exercise.equipment}',
-                  style: textStyle,
-                ),
+                DetailPageInfoCard(text: 'Equipments: ${exercise.equipment}'),
                 const SizedBox(
                   height: 8,
                 ),
-                Text(
-                  'Difficulty: ${exercise.difficulty}',
-                  style: textStyle,
-                ),
+                DetailPageInfoCard(text: 'Difficulty: ${exercise.difficulty}'),
                 const SizedBox(
                   height: 8,
                 ),
                 Container(
-                  padding: const EdgeInsets.all(8),
-                  decoration: const BoxDecoration(
-                    color: AppColor.kBlack,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(14),
-                      bottomRight: Radius.circular(14),
-                    ),
-                  ),
+                  padding: const EdgeInsets.symmetric(vertical: 8),
+                  // decoration: const BoxDecoration(
+                  //   color: AppColor.kBlack,
+                  //   borderRadius: BorderRadius.only(
+                  //     topLeft: Radius.circular(14),
+                  //     bottomRight: Radius.circular(14),
+                  //   ),
+                  // ),
                   child: Text(
                     exercise.instructions,
                     style: textStyle.copyWith(
@@ -89,6 +60,28 @@ class DetailPage extends StatelessWidget {
           ),
         ),
       ),
+    );
+  }
+}
+
+class DetailPageInfoCard extends StatelessWidget {
+  const DetailPageInfoCard({required this.text, super.key});
+  final String text;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: AppColor.kPrimary,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Row(crossAxisAlignment: CrossAxisAlignment.center, children: [
+        const Icon(Icons.arrow_right_rounded, size: 28, color: AppColor.kBlack),
+        Text(text,
+            style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+                color: AppColor.kBlack))
+      ]),
     );
   }
 }
